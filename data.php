3d1b8e3302bfcafaaa046a5b1721d6d4d4cfaaa3
Gittan Kaus <GittanKaus@users.noreply.github.com>
2016-10-13 11:16:43 +0300

    added 5

---
+++ data.php
@@ -0,0 +1,109 @@
+<?php 
+	// et saada ligi sessioonile
+	require("functions.php");
+	
+	//ei ole sisseloginud, suunan login lehele
+	if(!isset ($_SESSION["userId"])) {
+		header("Location: login.php");
+	}
+	
+	
+	//kas kasutaja tahab välja logida
+	// kas aadressireal on logout olemas
+	if (isset($_GET["logout"])) {
+		
+		session_destroy();
+		
+		header("Location: login.php");
+		
+	}
+	
+	if (	isset($_POST["note"]) && 
+			isset($_POST["color"]) && 
+			!empty($_POST["note"]) && 
+			!empty($_POST["color"]) 
+	) {
+		
+		$note = cleanInput($_POST["note"]);
+		
+		saveNote($note, $_POST["color"]);
+		
+	}
+	
+	$notes = getAllNotes();
+	
+	//echo "<pre>";
+	//var_dump($notes);
+	//echo "<pre>";
+	
+?>
+
+
+<h1>Data</h1>
+<p>
+	Tere tulemast <?=$_SESSION["userEmail"];?>!
+	<a href="?logout=1">Logi välja</a>
+</p>
+<h2>Märkmed</h2>
+<form method="POST">
+			
+	<label>Märkus</label><br>
+	<input name="note" type="text">
+	
+	<br><br>
+	
+	<label>Värv</label><br>
+	<input name="color" type="color">
+				
+	<br><br>
+	
+	<input type="submit">
+
+</form>
+
+<h2>arhiiv</h2>
+
+<?php
+
+	foreach ($notes as $n){
+		
+		$style = "width:200px; min-height:200px; border: 1px solid gray; background-color: ".$n->noteColor.";";
+		
+		echo "<p style=' ".$style."  '>".$n->note."</p>";
+		
+	}
+
+
+
+?>
+
+
+<h2 style="clear:both;">Tabel</h2>
+<?php
+
+	$html = "<table>";
+	
+		$html .= "<tr>";
+			$html .= "<th>id</th>";
+			$html .= "<th>Märkus</th>";
+			$html .= "<th>Värv</th>";
+		$html .= "</tr>";
+	foreach ($notes as $note){
+		$html .= "<tr>";
+			$html .= "<td>".$note->id."</td>";
+			$html .= "<td>".$note->note."</td>";
+			$html .= "<td>".$note->noteColor."</td>";
+		$html .= "</tr>";
+	}
+	
+	$html .= "</table>";
+	
+	echo $html;
+
+
+
+
+
+
+
+?>
