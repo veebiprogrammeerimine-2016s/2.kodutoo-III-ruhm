0b7f427735d501d940e5b39ad8af7c9b25f715ce
Kristjan Liiva <Zargorath@users.noreply.github.com>
2016-10-13 11:19:45 +0300

    Added files to use for homework

---
+++ data.php
@@ -0,0 +1,91 @@
+<?php 
+	// et saada ligi sessioonile
+	require("functions.php");
+	
+	//ei ole sisseloginud, suunan login lehele
+	if(!isset ($_SESSION["userId"])) {
+		header("Location: login.php");
+		exit();
+	}
+	
+	
+	//kas kasutaja tahab välja logida
+	// kas aadressireal on logout olemas
+	if (isset($_GET["logout"])) {
+		
+		session_destroy();
+		
+		header("Location: login.php");
+		
+	}
+	
+	
+	$notes = getAllNotes();
+
+	echo "<pre>";
+	var_dump($notes[1]->noteColor);
+	echo "</pre>";
+?>
+
+<h1>Data</h1>
+<p>
+	Tere tulemast <?=$_SESSION["userEmail"];?>!
+	<a href="?logout=1">Logi välja</a>
+</p>
+
+
+
+<h2>Märkmed</h2>
+<form method="POST">
+			
+			<label>Märkus</label><br>
+			<input name="note" type="text">
+			
+			<br><br>
+			
+			<label>Värv</label><br>
+			<input name="color" type="color">
+						
+			<br><br>
+			
+			<input type="submit">
+		
+		</form>
+		
+<h2>Arhiiv</h2>
+<?php
+
+	//iga liikme kohta massiivis
+	foreach ($notes as $n) {
+		
+		$style = "width:150px; min-height:200px; border: 1px solid gray; background-color:".$n->noteColor.";";
+		echo"<p style='".$style."'>".$n->note."</p>";
+		
+	}
+
+?>
+
+<h2 style="clear:both;">Tabel</h2>
+<?php
+	
+	$html = "<table>";
+		
+		$html .= "<tr>";
+			$html .= "<th>id</th>";
+			$html .= "<th>Märkus</th>";
+			$html .= "<th>Värv</th>";
+		$html .= "</tr>";
+	
+	foreach ($notes as $note) {
+		$html .= "<tr>";
+			$html .= "<td>".$note->id."</td>";
+			$html .= "<td>".$note->note."</td>";
+			$html .= "<td>".$note->noteColor."</td>";
+		$html .= "</tr>";
+	}
+
+	$html .= "</table>";
+	
+	echo $html;
+
+?>
---
+++ functions.php
@@ -0,0 +1,190 @@
+<?php 
+	// functions.php
+	require("../../config.php");
+	// et saab kasutada $_SESSION muutujaid
+	// kõigis failides mis on selle failiga seotud
+	session_start();
+	
+	
+	$database = "if16_kliiva";
+	
+	//var_dump($GLOBALS);
+	
+	function signup($email, $password) {
+		
+		$mysqli = new mysqli(
+		
+		$GLOBALS["serverHost"], 
+		$GLOBALS["serverUsername"],  
+		$GLOBALS["serverPassword"],  
+		$GLOBALS["database"]
+		
+		);
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+		echo $mysqli->error;
+		
+		$stmt->bind_param("ss", $email, $password );
+		if ( $stmt->execute() ) {
+			echo "salvestamine õnnestus";	
+		} else {	
+			echo "ERROR ".$stmt->error;
+		}
+		
+	}
+	
+	
+	function login($email, $password) {
+		
+		$notice = "";
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"],  $GLOBALS["serverPassword"],  $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("
+		
+			SELECT id, email, password, created
+			FROM user_sample
+			WHERE email = ?
+		
+		");
+		// asendan ?
+		$stmt->bind_param("s", $email);
+		
+		// määran muutujad reale mis kätte saan
+		$stmt->bind_result($id, $emailFromDb, $passwordFromDb, $created);
+		
+		$stmt->execute();
+		
+		// ainult SLECTI'i puhul
+		if ($stmt->fetch()) {
+			
+			// vähemalt üks rida tuli
+			// kasutaja sisselogimise parool räsiks
+			$hash = hash("sha512", $password);
+			if ($hash == $passwordFromDb) {
+				// õnnestus 
+				echo "Kasutaja ".$id." logis sisse";
+				
+				$_SESSION["userId"] = $id;
+				$_SESSION["userEmail"] = $emailFromDb;
+				
+				header("Location: data.php");
+				exit();
+				
+			} else {
+				$notice = "Vale parool!";
+			}
+			
+		} else {
+			// ei leitud ühtegi rida
+			$notice = "Sellist emaili ei ole!";
+		}
+		
+		return $notice;
+	}
+	
+	function getAllNotes() {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"],  $GLOBALS["serverPassword"],  $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("SELECT id, note, color FROM colorNotes");
+		
+		$stmt->bind_result($id, $note, $color);
+		$stmt->execute();
+		
+		$result = array();
+		
+		while($stmt->fetch()) {
+			//echo $note."<br>";
+			
+			$object = new StdClass();
+			$object->id = $id;
+			$object->note = $note;
+			$object->noteColor = $color;
+			
+			array_push($result, $object);
+		}
+		
+		return $result;
+		
+	}
+	function saveNote($note, $color) {
+		
+		$mysqli = new mysqli(
+		
+		$GLOBALS["serverHost"], 
+		$GLOBALS["serverUsername"],  
+		$GLOBALS["serverPassword"],  
+		$GLOBALS["database"]
+		
+		);
+		$stmt = $mysqli->prepare("INSERT INTO colorNotes (note, color) VALUES (?, ?)");
+		echo $mysqli->error;
+		
+		$stmt->bind_param("ss", $note, $color );
+		if ( $stmt->execute() ) {
+			echo "salvestamine õnnestus";	
+		} else {	
+			echo "ERROR ".$stmt->error;
+		}
+		
+	}
+	
+	if (	isset($_POST["note"]) && 
+			isset($_POST["color"]) && 
+			!empty($_POST["note"]) && 
+			!empty($_POST["color"]) 
+	) {
+		
+		$note = cleanInput($_POST["note"]);
+		
+		saveNote($note, $_POST["color"]);
+		
+	}
+	
+	function cleanInput ($input) {
+		
+		//algul - " tere tulemast "
+		$input = trim($input);
+		//sellega koos - "tere tulemast"
+		
+		$input = stripslashes($input);
+		
+		//"<"
+		$input = htmlspecialchars($input);
+		//"&lt"
+		
+		return $input;
+	}
+	
+	
+	
+	
+	
+	/*function sum($x, $y) {
+		
+		$answer = $x+$y;
+		
+		return $answer;
+	}
+	
+	function hello($firstname, $lastname) {
+		
+		return 
+		"Tere tulemast "
+		.$firstname
+		." "
+		.$lastname
+		."!";
+		
+	}
+	
+	echo sum(123123789523,1239862345);
+	echo "<br>";
+	echo sum(1,2);
+	echo "<br>";
+	
+	$firstname = "Kristjan";
+	
+	echo hello($firstname, "L.");
+	*/
+?>
